ffa6b8fddd601673031579c83b64cc6ac7966403
rieaaddlreiuu <133306044+rieaaddlreiuu@users.noreply.github.com>
2023-10-04 07:46:39 +0000

    dbの調整

---
+++ replit.nix
@@ -0,0 +1,6 @@
+{ pkgs }: {
+	deps = [
+		pkgs.sqlite.bin
+  pkgs.php82
+	];
+}
--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 create table panel(
-    id int(11) primary key auto_increment,
+    id integer primary key autoincrement,
     width int(11) not null,
     height int(11) not null,
     pic_path varchar(127) not null,
@@ -7,7 +7,7 @@ create table panel(
 );
 
 create table panel_chk(
-    id int(11) primary key auto_increment,
+    id integer primary key autoincrement,
     panel int(11) not null,
     num int(11) not null
 );
