dd5a1f3365871f95c669074e1474dc31ec2af47f
iise2xqyz <iise2xqyz@gmail.com>
2023-10-04 15:45:50 +0900

    dbつくるよ

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 create table panel(
     id int(11) primary key auto_increment,
-    widt int(11) not null,
+    width int(11) not null,
     height int(11) not null,
     pic_path varchar(127) not null,
+    name varchar(127)
 );
